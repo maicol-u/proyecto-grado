6f298db59d5580890e9fe2d60d4c206aa05d3484
Michael Ubaque <maikolubaque@gmail.com>
2025-12-15 15:33:15 -0500

    Inicializar repositorio Laravel

---
+++ database/migrations/2025_12_15_202430_create_usuario_invernaderos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('usuario_invernaderos', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_usuario');
+            $table->unsignedBigInteger('id_invernadero');
+            $table->primary(['id_usuario', 'id_invernadero']);
+            $table->foreign('id_usuario')->references('id')->on('usuarios');
+            $table->foreign('id_invernadero')->references('id')->on('invernaderos')->onDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('usuario_invernaderos');
+    }
+};
